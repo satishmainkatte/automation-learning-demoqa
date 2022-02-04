cc12260cb0b4b3806256a5dfee73ff8508b389e2
smainkatte <smainkatte@xento.com>
2022-02-04 18:47:18 +0530

    added webdrivermanager to perform cross browser tests and removed local browser driver dependency

--- src/test/java/setup/BaseTest.java
+++ src/test/java/setup/BaseTest.java
@@ -3,20 +3,14 @@ package setup;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class BaseTest {
     public static WebDriver driver;
-    private static String browserDriver;
-    private static String url;
 
     @BeforeClass
     public static void entryLoader(){
         System.out.println("Webdriver setup going on...");
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = WebDriverManagerTest.browserDriverSetup("Chrome");
         System.out.println("Webdiver created!!");
         driver.manage().window().maximize();
         driver.get("https://demoqa.com/");
